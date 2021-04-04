bf0a2e6e7195c9aecafd2562b82345d85210b26a
w-haibara <hwhaibarawataru@gmail.com>
2021-04-05 01:25:48 +0900

    add eval

--- eval.go
+++ eval.go
@@ -3,6 +3,7 @@ package unlambda
 import (
 	"fmt"
 	"io"
+	"reflect"
 )
 
 type Env struct {
@@ -27,12 +28,174 @@ func (env Env) EvalString(expr string) error {
 		return err
 	}
 
-	fmt.Fprint(env.Out, "a")
-
 	return nil
 }
 
 func (env Env) eval(n *node) error {
+	fmt.Fprintln(env.Err, n)
 
-	return nil
+	if n == nil {
+		return nil
+	}
+
+	if n.isLeaf() {
+		return nil
+	}
+
+	if n.v != symbolAplly {
+		return nil
+	}
+
+	switch string(n.l.v[0]) {
+	case symbolAplly:
+		switch {
+		case !n.l.isLeaf() && string(n.l.l.v[0]) == symbolS:
+			if n.l.r.isLeaf() && n.r.isLeaf() {
+				return nil
+			}
+
+			old := n
+
+			if !n.l.r.isLeaf() {
+				if err := env.eval(n.l.r); err != nil {
+					return err
+				}
+			}
+
+			if !n.r.isLeaf() {
+				if err := env.eval(n.r); err != nil {
+					return err
+				}
+			}
+
+			if reflect.DeepEqual(old, n) {
+				return nil
+			}
+
+			if err := env.eval(n); err != nil {
+				return err
+			}
+
+			return nil
+		case !n.l.isLeaf() && !n.l.l.isLeaf() && string(n.l.l.l.v[0]) == symbolS:
+			fmt.Fprintln(env.Err, "=== eval parameter 1 of s ===")
+			if err := env.eval(n.l.l.r); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(env.Err, "=== eval parameter 2 of s ===")
+			if err := env.eval(n.l.r); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(env.Err, "=== eval parameter 3 of s ===")
+			if err := env.eval(n.r); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(env.Err, "=== apply s ===")
+			fmt.Fprint(env.Err, n, " --> ")
+			env.s(n)
+			fmt.Fprintln(env.Err, n)
+			fmt.Fprintln(env.Err, "=== break s ===")
+
+			if err := env.eval(n); err != nil {
+				return err
+			}
+
+			return nil
+		case !n.l.isLeaf() && string(n.l.l.v[0]) == symbolK:
+			fmt.Fprintln(env.Err, "=== eval parameter 1 of k ===")
+			if err := env.eval(n.l.r); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(env.Err, "=== eval parameter 2 of k ===")
+			if err := env.eval(n.r); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(env.Err, "=== apply k ===")
+			fmt.Fprint(env.Err, n, " --> ")
+			env.k(n)
+			fmt.Fprintln(env.Err, n)
+			fmt.Fprintln(env.Err, "=== break k ===")
+
+			if err := env.eval(n); err != nil {
+				return err
+			}
+
+			return nil
+		default:
+			if err := env.eval(n.l); err != nil {
+				return err
+			}
+
+			if err := env.eval(n); err != nil {
+				return err
+			}
+
+			return nil
+		}
+	case symbolS:
+		if n.r.isLeaf() {
+			return nil
+		}
+
+		if err := env.eval(n.r); err != nil {
+			return err
+		}
+
+		return nil
+	case symbolK:
+		if n.r.isLeaf() {
+			return nil
+		}
+
+		if err := env.eval(n.r); err != nil {
+			return err
+		}
+
+		return nil
+	case symbolI:
+		if err := env.eval(n.r); err != nil {
+			return err
+		}
+
+		env.i(n)
+
+		if err := env.eval(n); err != nil {
+			return err
+		}
+
+		return nil
+	case symbolDotX:
+		if err := env.eval(n.r); err != nil {
+			return err
+		}
+
+		env.dotX(n)
+
+		if err := env.eval(n); err != nil {
+			return err
+		}
+
+		return nil
+	case symbolR:
+		if err := env.eval(n.r); err != nil {
+			return err
+		}
+
+		env.r(n)
+
+		if err := env.eval(n); err != nil {
+			return err
+		}
+
+		return nil
+	default:
+		return fmt.Errorf("unknown symbol[%s]", string(n.l.v[0]))
+	}
+
+	return fmt.Errorf("unknown error")
 }
--- eval_test.go
+++ eval_test.go
@@ -10,89 +10,151 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Eval(t *testing.T) {
+func Test_eval(t *testing.T) {
 	testCases := []struct {
-		in  string
-		out string
+		in      string
+		inAfter string
+		out     string
+		err     error
 	}{
+
 		// function p
 		{
-			in:  "`.a.b",
-			out: "a",
+			in:      "`.a.b",
+			inAfter: ".b",
+			out:     "a",
+			err:     nil,
 		},
-		/*
-			{
-				in:  "`.ai",
-				out: "a",
-			},
-			{
-				in:  "`````````````.H.e.l.l.o.,. .w.o.r.l.d.!i",
-				out: "Hello, world!",
-			},
-			{
-				in:  "`.a`.c.d",
-				out: "ca",
-			},
-			{
-				in:  "``.a.b`.c.d",
-				out: "acb",
-			},
-			// function r
-			{
-				in:  "`ri",
-				out: "\n",
-			},
-			{
-				in:  "```.ar.bi",
-				out: "a\nb",
-			},
-			// function k
-			{
-				in:  "``ki`.ai",
-				out: "a",
-			},
-			{
-				in:  "```k.aii",
-				out: "a",
-			},
-			{
-				in:  "```k.b`.aii",
-				out: "ab",
-			},
-			{
-				in:  "```k.a`k.bi",
-				out: "a",
-			},
-			// function s
-			{
-				in:  "``s`.ai`.bi",
-				out: "ab",
-			},
-			{
-				in:  "```s.a.b.c",
-				out: "abc",
-			},
-			{
-				in:  "````skk.ai",
-				out: "a",
-			},
-			{
-				in:  "```si`ki.a",
-				out: "a",
-			},
-			{
-				in:  "```sii```skk.b",
-				out: "b",
-			},
-			{
-				in:  "```sii`.a.b",
-				out: "ab",
-			},
-			{
-				in:  "``si`k.a",
-				out: "",
-			},
-		*/
+		{
+			in:      "`.ai",
+			inAfter: "i",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "`````````````.H.e.l.l.o.,. .w.o.r.l.d.!i",
+			inAfter: "i",
+			out:     "Hello, world!",
+			err:     nil,
+		},
+		{
+			in:      "`.a`.c.d",
+			inAfter: ".d",
+			out:     "ca",
+			err:     nil,
+		},
+		{
+			in:      "``.a.b`.c.d",
+			inAfter: ".d",
+			out:     "acb",
+			err:     nil,
+		},
+		// function r
+		{
+			in:      "`ri",
+			inAfter: "i",
+			out:     "\n",
+			err:     nil,
+		},
+		{
+			in:      "```.ar.bi",
+			inAfter: "i",
+			out:     "a\nb",
+			err:     nil,
+		},
+		// function i
+		{
+			in:      "`i.a",
+			inAfter: ".a",
+			out:     "",
+			err:     nil,
+		},
+		{
+			in:      "``i`ii.a",
+			inAfter: ".a",
+			out:     "",
+			err:     nil,
+		},
+		// function k
+		{
+			in:      "``k.x`.a.y",
+			inAfter: ".x",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "```k.aii",
+			inAfter: "i",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "```k.b`.aii",
+			inAfter: "i",
+			out:     "ab",
+			err:     nil,
+		},
+		{
+			in:      "```k.a`k.bi",
+			inAfter: "i",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "`k`ki",
+			inAfter: "`k`ki",
+			out:     "",
+			err:     nil,
+		},
+		// function s
+		{
+			in:      "``s`.ai`.bi",
+			inAfter: "``sii",
+			out:     "ab",
+			err:     nil,
+		},
+		{
+			in:      "```s.a.b.c",
+			inAfter: ".c",
+			out:     "abc",
+			err:     nil,
+		},
+		{
+			in:      "````skk.ai",
+			inAfter: "i",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "```si`ki.a",
+			inAfter: "i",
+			out:     "a",
+			err:     nil,
+		},
+		{
+			in:      "```sii```skk.b",
+			inAfter: ".b",
+			out:     "b",
+			err:     nil,
+		},
+		{
+			in:      "```sii`.a.b",
+			inAfter: ".b",
+			out:     "ab",
+			err:     nil,
+		},
+		{
+			in:      "``si`k.a",
+			inAfter: "``si`k.a",
+			out:     "",
+			err:     nil,
+		},
+		{
+			in:      "```s``si`k.*`ki```s``s`k``s`ksk``sii``si`k``s``s`kski``s``s`ksk``s``s`kski",
+			inAfter: "i",
+			out:     "************************************************************************************************************************************************************************************************************************",
+			err:     nil,
+		},
 	}
 
 	f, err := os.Create("test.log")
@@ -102,6 +164,7 @@ func Test_Eval(t *testing.T) {
 	defer f.Close()
 
 	fr := bufio.NewWriter(f)
+	defer fr.Flush()
 
 	for i, testCase := range testCases {
 		buf := new(bytes.Buffer)
@@ -115,11 +178,22 @@ func Test_Eval(t *testing.T) {
 		fmt.Fprintln(fr, i, testCase.in)
 		fmt.Println(i, testCase.in)
 
-		env.EvalString(testCase.in)
+		token, err := tokenize(testCase.in)
+		if err != nil {
+			panic(err)
+		}
+
+		node, err := token.parse()
+		if err != nil {
+			panic(err)
+		}
+
+		err = env.eval(node)
 
 		fmt.Fprint(fr, "--------------------------------------------\n\n")
-		fr.Flush()
 
+		assert.Equal(t, testCase.err, err)
 		assert.Equal(t, testCase.out, buf.String())
+		assert.Equal(t, testCase.inAfter, node.String())
 	}
 }
